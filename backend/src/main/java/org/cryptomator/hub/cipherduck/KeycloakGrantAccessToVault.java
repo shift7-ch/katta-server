e183001636db10bbca4756ac598b09366541289a
chenkins <chenkins44@gmail.com>
2023-07-03 17:41:17 +0200

    Simplify concat

--- backend/src/main/java/org/cryptomator/hub/cipherduck/KeycloakGrantAccessToVault.java
+++ backend/src/main/java/org/cryptomator/hub/cipherduck/KeycloakGrantAccessToVault.java
@@ -1,20 +1,17 @@
 package org.cryptomator.hub.cipherduck;
 
 import org.cryptomator.hub.SyncerConfig;
-import org.jboss.logging.Logger;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.representations.idm.UserRepresentation;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
 public class KeycloakGrantAccessToVault {
-    private static final Logger LOG = Logger.getLogger(KeycloakGrantAccessToVault.class);
 
 
     public static void keycloakGrantAccessToVault(final SyncerConfig syncerConfig, final String vaultId, final String userId) {
@@ -26,7 +23,7 @@ public class KeycloakGrantAccessToVault {
 
             // TODO do we want to use user attributes? Or should we use groups/....? What happens in federation setting - do attributes come from AD etc. and will there be no conflict?
             final Map<String, List<String>> attributes = ur.getAttributes();
-            attributes.put("vaults", Stream.concat(attributes.getOrDefault("vaults", Collections.EMPTY_LIST).stream(), Arrays.asList(vaultId).stream()).toList());
+            attributes.put("vaults", Stream.concat(attributes.getOrDefault("vaults", Collections.EMPTY_LIST).stream(), Stream.of(vaultId)).toList());
             ur.setAttributes(attributes);
             userResource.update(ur);
         }
