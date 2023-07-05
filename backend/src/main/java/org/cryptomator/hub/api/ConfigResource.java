714d6f9465988feccd4bc191297f24e1445b3e86
chenkins <chenkins44@gmail.com>
2023-07-05 15:02:41 +0200

    Config cipherduck-staging (one role for all buckets).

--- backend/src/main/java/org/cryptomator/hub/api/ConfigResource.java
+++ backend/src/main/java/org/cryptomator/hub/api/ConfigResource.java
@@ -62,10 +62,10 @@ public class ConfigResource {
 	@Operation(summary = "get cipherduckprofile for this hub")
 	public String cipherduckprofile() throws IOException {
 		// TODO what kind of downstream pattern should we use (https://pubs.opengroup.org/architecture/o-aa-standard/DDD-strategic-patterns.html) - close to cyberduck/mountainduck or de-couple? Which representation: XML/JSON...?
+		// DK: I would design the API to return a custom model and not resuse the *.cyberduckprofile XML
 		// TODO which properties do we need to make configurable and inject?
-		return new String(ConfigResource.class.getResourceAsStream("/cipherduck/S3-MinIO-STS-cryptomator-localhost.cyberduckprofile").readAllBytes());
-		// TODO test AWS
-//		return new String(ConfigResource.class.getResourceAsStream("/cipherduck/S3-AWS-STS-cryptomator-staging.cyberduckprofile").readAllBytes());
+//		return new String(ConfigResource.class.getResourceAsStream("/cipherduck/S3-MinIO-STS-cryptomator-localhost.cyberduckprofile").readAllBytes());
+		return new String(ConfigResource.class.getResourceAsStream("/cipherduck/S3-AWS-STS-cryptomator-staging.cyberduckprofile").readAllBytes());
 	}
 
 	//visible for testing
