b7382ce77072a76281069555bc5edf3f9acb4d1e
chenkins <chenkins44@gmail.com>
2023-08-02 10:17:00 +0200

    Implement cipherduck hub bookmark download frontend page (#16).

--- frontend/src/common/backend.ts
+++ frontend/src/common/backend.ts
@@ -381,6 +381,12 @@ class StorageService {
     return axiosAuth.put('/storage/', dto);
   }
 }
+class ConfigService {
+  public async cipherduckhubbookmark(): Promise<string> {
+    const axiosUnAuth = AxiosStatic.create(axiosBaseCfg);
+    return axiosUnAuth.get('/config/cipherduckhubbookmark');
+  }
+}
 // \ end cipherduck extension
 
 /**
@@ -396,6 +402,7 @@ const services = {
 
   // / start cipherduck extension
   ,storage: new StorageService()
+  ,config: new ConfigService()
   // \ end cipherduck extension
 };
 
