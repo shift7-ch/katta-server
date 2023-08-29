a146edca2c6844256600b162772ce0e8f0576824
chenkins <chenkins44@gmail.com>
2023-08-29 11:28:14 +0200

    Fix failing tests as Keycloak is not available at quarkus test time.

--- backend/src/main/java/org/cryptomator/hub/cipherduck/KeycloakGrantAccessToVault.java
+++ backend/src/main/java/org/cryptomator/hub/cipherduck/KeycloakGrantAccessToVault.java
@@ -1,6 +1,7 @@
 package org.cryptomator.hub.cipherduck;
 
 import org.cryptomator.hub.SyncerConfig;
+import org.jboss.logging.Logger;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.admin.client.resource.UserResource;
@@ -13,9 +14,16 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 public class KeycloakGrantAccessToVault {
-
+    private static final Logger LOG = Logger.getLogger(KeycloakGrantAccessToVault.class);
 
     public static void keycloakGrantAccessToVault(final SyncerConfig syncerConfig, final String vaultId, final String userId) {
+        LOG.info("keycloakURL=" + syncerConfig.getKeycloakUrl());
+        // N.B. quarkus has no means to provide empty string as value, interpreted as no value, see https://github.com/quarkusio/quarkus/issues/2765
+        // TODO better solution than using sentinel string "empty"?
+        if ("empty".equals(syncerConfig.getKeycloakUrl())) {
+            LOG.error(String.format("Could not grant access to vault %s for user %s as keycloak URL is not defined.", vaultId, userId));
+            return;
+        }
         try (final Keycloak keycloak = Keycloak.getInstance(syncerConfig.getKeycloakUrl(), syncerConfig.getKeycloakRealm(), syncerConfig.getUsername(), syncerConfig.getPassword(), syncerConfig.getKeycloakClientId())) {
 
             final RealmResource realm = keycloak.realm(syncerConfig.getKeycloakRealm());
@@ -24,7 +32,7 @@ public class KeycloakGrantAccessToVault {
 
             // TODO https://github.com/chenkins/cipherduck-hub/issues/4 do we want to use user attributes? Or should we use groups/....? What happens in federation setting - do attributes come from AD etc. and will there be no conflict?
             Map<String, List<String>> attributes = ur.getAttributes();
-            if(attributes == null){
+            if (attributes == null) {
                 attributes = new HashMap<>();
             }
             attributes.put("vault", Stream.concat(attributes.getOrDefault("vault", Collections.EMPTY_LIST).stream(), Stream.of(vaultId)).toList());
