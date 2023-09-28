d1ffe6419860da8d99cac6bd297a1c30969201f0
chenkins <chenkins44@gmail.com>
2023-09-28 10:17:37 +0200

    Add hub frontend vault storage configuration for STS (MinIO + AWS) (#3).

--- frontend/src/components/CreateVaultS3.vue
+++ frontend/src/components/CreateVaultS3.vue
@@ -365,8 +365,6 @@ async function createVault() {
         // TODO https://github.com/chenkins/cipherduck-hub/issues/3 safe - side-effects?
         config["jwe"]["stsRoleArn"] = config["stsRoleArnPrefix"]  + bucketName
     }
-    // TODO https://github.com/chenkins/cipherduck-hub/issues/3 how do we use vendor?
-    config["jwe"]["vendor"] = config["id"];
     // \ end cipherduck extension
     const ownerJwe = await vaultKeys.value.encryptForUser(base64.parse(owner.publicKey)
       // / start cipherduck extension
