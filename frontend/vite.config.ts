09d8918d0cdd09889a8736c8a1f297795d32214f
chenkins <chenkins44@gmail.com>
2023-11-07 11:54:29 +0100

    Remove proxyman stuff again as not used.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -33,8 +33,6 @@ export default defineConfig({
     port: 3000,
     strictPort: true,
     proxy: {
-      // uncomment to use proxyman.local setup:
-      //'/api/': 'http://proxyman.local:8080'
       '/api/': 'http://127.0.0.1:8080'
     }
   }
