7a42a8c0b4fbbe8ec1e132a60064224ba363d123
chenkins <chenkins44@gmail.com>
2023-11-25 22:32:37 +0100

    Get full region list from AWS SDK instead of hard-coding (code review overheadhunter).

--- backend/src/main/java/org/cryptomator/hub/api/cipherduck/StorageConfigDto.java
+++ backend/src/main/java/org/cryptomator/hub/api/cipherduck/StorageConfigDto.java
@@ -1,5 +1,7 @@
 package org.cryptomator.hub.api.cipherduck;
 
+import com.amazonaws.regions.Regions;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -22,36 +24,7 @@ public record StorageConfigDto(
 		// workaround for defaultValue in JSONPrroperty not working as expected
 		this(s.id(), s.name(), s.bucketPrefix(), s.stsRoleArnClient(), s.stsRoleArnHub(), s.stsEndpoint(),
 				s.region().isPresent() ? s.region() : Optional.of("us-east-1"),
-				s.regions().isPresent() ? s.regions() : Optional.of(Arrays.asList(
-						"af-south-1",
-						"ap-east-1",
-						"ap-south-1",
-						"ap-south-2",
-						"ap-northeast-1",
-						"ap-northeast-2",
-						"ap-northeast-3",
-						"ap-southeast-1",
-						"ap-southeast-2",
-						"ap-southeast-3",
-						"ca-central-1",
-						"cn-north-1",
-						"cn-north-1",
-						"eu-west-1",
-						"eu-west-2",
-						"eu-west-3",
-						"eu-north-1",
-						"eu-south-1",
-						"eu-south-2",
-						"eu-central-1",
-						"eu-central-2",
-						"me-central-1",
-						"me-south-1",
-						"sa-east-1",
-						"us-east-1",
-						"us-east-2",
-						"us-west-1",
-						"us-west-2"
-				)),
+				s.regions().isPresent() ? s.regions() : Optional.of(Arrays.stream(Regions.values()).map(r -> r.getName()).toList()),
 				jwe, s.withPathStyleAccessEnabled(), s.s3Endpoint());
 	}
 
