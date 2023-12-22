02decf8be51fb4a83d2f36ab7c48737cb71a3979
chenkins <chenkins44@gmail.com>
2023-12-22 13:47:30 +0100

    Fix cipherduck start/end extension markers.

--- frontend/src/components/CreateVault.vue
+++ frontend/src/components/CreateVault.vue
@@ -315,12 +315,16 @@
                 {{ t('CreateVaultS3.success.vaultPermanenDownloadVaultTemplate') }}
               </p>
             </div>
+            <!-- \ end cipherduck extension -->
+
             <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-primary hover:bg-primary-d1 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary" @click="downloadVaultTemplate()">
                 <ArrowDownTrayIcon class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
                 {{ t('createVault.success.download') }}
             </button>
             <p v-if="onDownloadTemplateError != null " class="text-sm text-red-900 mr-4">{{ t('createVault.error.downloadTemplateFailed', [onDownloadTemplateError.message]) }}</p> <!-- TODO: not beautiful-->
         </div>
+
+        <!-- / start cipherduck extension -->
         <div v-if="isPermanent" class="mt-5 sm:mt-6">
             <div class="mt-2">
               <p class="text-sm text-gray-500">
