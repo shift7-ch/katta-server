106978e5f2832d3366b55820df94db1d6f264a28
chenkins <chenkins44@gmail.com>
2024-04-03 14:50:00 +0200

    Link to admin setup documentation in github (#44).

--- frontend/src/components/cipherduck/StorageProfiles.vue
+++ frontend/src/components/cipherduck/StorageProfiles.vue
@@ -13,6 +13,24 @@
   <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
     {{ t('storageProfileList.title') }}
   </h2>
+  <div class="space-y-6 mt-5">
+    <div class="bg-white px-4 py-5 shadow sm:rounded-lg sm:p-6">
+      <div class="md:grid md:grid-cols-3 md:gap-6">
+        <div class="md:col-span-3">
+          <h3 class="text-lg font-medium leading-6 text-gray-900">
+            {{ t('storageProfileList.setup.title') }}
+          </h3>
+          <div class="col-span-6 sm:col-span-3">
+            <p id="keycloakAdminRealmURL" class="inline-flex mt-2 text-sm">
+              <LinkIcon class="shrink-0 text-primary mr-1 h-5 w-5" aria-hidden="true" />
+              <!-- TODO final URL? -->
+              <a role="button" href="https://github.com/shift7-ch/cipherduck-hub/blob/feature/cipherduck/backend/CIPHERDUCK.md" target="_blank" class="underline text-gray-500 hover:text-gray-900">{{ $t('storageProfileList.setup.description') }}</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <div class="pb-5 mt-3 border-b border-gray-200 flex flex-wrap sm:flex-nowrap gap-3 items-center whitespace-nowrap">
     <input id="storageprofileSearch" v-model="query" :placeholder="t('storageProfileList.search.placeholder')" type="text" class="focus:ring-primary focus:border-primary block w-full shadow-sm text-sm border-gray-300 rounded-md disabled:bg-gray-200"/>
@@ -60,7 +78,7 @@
 </template>
 
 <script setup lang="ts">
-import { ChevronRightIcon } from '@heroicons/vue/24/solid';
+import { ChevronRightIcon, LinkIcon } from '@heroicons/vue/24/solid';
 import { computed, nextTick, onMounted, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import auth from '../../common/auth';
