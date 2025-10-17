c2032dd723b1e1a73cb5362d670b61ff1ae90788
chenkins <chenkins44@gmail.com>
2025-10-17 11:00:50 +0200

    Add inspect refresh with offline access respecting offline lifespan.

--- keycloak/src/test/java/cloud/katta/KeycloakSessionAndTokenTimeoutsSandboxIT.java
+++ keycloak/src/test/java/cloud/katta/KeycloakSessionAndTokenTimeoutsSandboxIT.java
@@ -210,7 +210,7 @@ public class KeycloakSessionAndTokenTimeoutsSandboxIT {
 			// let session expire
 			Thread.sleep(ssoSessionMaxLifespanSeconds * 1000L);
 
-			// after session expiry, refresh with offline token ist still possible
+			// after session expiry, refresh with offline token is still possible
 			given()
 					.header("Content-Type", "application/x-www-form-urlencoded")
 					.formParam("client_id", "cryptomator")
@@ -220,6 +220,101 @@ public class KeycloakSessionAndTokenTimeoutsSandboxIT {
 					.post(container.getAuthServerUrl() + "/realms/cryptomator/protocol/openid-connect/token")
 					.then()
 					.statusCode(200).extract();
+		}
+	}
+
+	/**
+	 *
+	 */
+	@ParameterizedTest
+	@CsvSource({"26.3.1,5,5"})
+	public void inspectRefreshWithOfflineAccessMaxOffline(final String keycloakVersion, final int ssoSessionMaxLifespanSeconds, final int offlineSessionMaxLifespan) throws JSONException, InterruptedException {
+		try (final KeycloakContainer container = new KeycloakContainer(String.format("quay.io/keycloak/keycloak:%s", keycloakVersion))
+				.withFeaturesEnabled("token-exchange", "admin-fine-grained-authz")
+				// comment in for local debugging:
+				//              .withDebugFixedPort(5005, false)
+				//              .withCustomCommand("--log-level=DEBUG")
+				// see https://github.com/dasniko/testcontainers-keycloak/blob/main/README.md
+				//     https://github.com/dasniko/keycloak-extensions-demo/blob/1523b9c153f4325373c8d6787bfeb6c95d3dfed8/docker-compose.yml#L25
+				.withRealmImportFile("/cryptomator-realm.json")
+				// Keycloak < 25 seems to expose /health/started on default port, and not on management port as expected in testcontainers-keycloak:
+				//   https://github.com/dasniko/testcontainers-keycloak/blame/d910aa6d6919c0e0f9cd50f97c9bf878eb24f753/src/main/java/dasniko/testcontainers/keycloak/ExtendableKeycloakContainer.java#L203
+				.waitingFor(Wait.forLogMessage(".*Listening.*", 1))
+				// N.B. remove once we're Keycloak >= 26, see https://github.com/dasniko/testcontainers-keycloak/issues/152
+				.withEnv("KEYCLOAK_ADMIN", "admin")
+				.withEnv("KEYCLOAK_ADMIN_PASSWORD", "admin")
+		) {
+			container.start();
+			System.out.println(container.getAuthServerUrl());
+
+			final Keycloak keycloak = Keycloak.getInstance(
+					container.getAuthServerUrl(),
+					"master",
+					"admin",
+					"admin",
+					"admin-cli");
+
+			// enable direct access grant for client cryptomator
+			final RealmResource realm = keycloak.realm("cryptomator");
+			final ClientRepresentation cryptomatorClient = realm.clients().findByClientId("cryptomator").getFirst();
+
+			cryptomatorClient.setDirectAccessGrantsEnabled(true);
+			keycloak.realm("cryptomator").clients().get(cryptomatorClient.getId()).update(cryptomatorClient);
+
+			// set ssoSessionMaxLifespan
+			final RealmRepresentation realmRepresentation = realm.toRepresentation();
+			realmRepresentation.setSsoSessionMaxLifespan(ssoSessionMaxLifespanSeconds); // seconds, see https://www.keycloak.org/docs-api/latest/javadocs/org/keycloak/models/RealmModel.html
+			realmRepresentation.setOfflineSessionMaxLifespanEnabled(true);
+			realmRepresentation.setOfflineSessionMaxLifespan(offlineSessionMaxLifespan); // seconds, see https://www.keycloak.org/docs-api/latest/javadocs/org/keycloak/models/RealmModel.html
+			realm.update(realmRepresentation);
+
+			// get access token and verify its expiry is smaller than ssoSessionMaxLifespan
+			final ExtractableResponse<io.restassured.response.Response> tokenResponse = given()
+					.header("Content-Type", "application/x-www-form-urlencoded")
+					.formParam("client_id", "cryptomator")
+					.formParam("grant_type", "password")
+					.formParam("username", "alice")
+					.formParam("password", "asd")
+					.formParam("scope", "offline_access")
+					.when()
+					.post(container.getAuthServerUrl() + "/realms/cryptomator/protocol/openid-connect/token")
+					.then()
+					.statusCode(200).extract();
+
+			final JSONObject accessToken = deocdeJWT(tokenResponse.path("access_token"));
+			final String refreshToken = tokenResponse.path("refresh_token");
+			final LocalDateTime expiry = LocalDateTime.ofEpochSecond(accessToken.getInt("exp"), 0, OffsetDateTime.now().getOffset());
+			final LocalDateTime now = LocalDateTime.now();
+
+			final long delta = ChronoUnit.SECONDS.between(now, expiry);
+			assert delta >= 0;
+			// TODO as expected vs. above.
+			assert delta < ssoSessionMaxLifespanSeconds;
+
+			// do a refresh within session lifetime
+			given()
+					.header("Content-Type", "application/x-www-form-urlencoded")
+					.formParam("client_id", "cryptomator")
+					.formParam("grant_type", "refresh_token")
+					.formParam("refresh_token", refreshToken)
+					.when()
+					.post(container.getAuthServerUrl() + "/realms/cryptomator/protocol/openid-connect/token")
+					.then()
+					.statusCode(200).extract();
+
+			// let offline access lifespan expire
+			Thread.sleep(offlineSessionMaxLifespan * 1000L);
+
+			// after offline access lifespan expire, refresh with offline token is not possible any more
+			given()
+					.header("Content-Type", "application/x-www-form-urlencoded")
+					.formParam("client_id", "cryptomator")
+					.formParam("grant_type", "refresh_token")
+					.formParam("refresh_token", refreshToken)
+					.when()
+					.post(container.getAuthServerUrl() + "/realms/cryptomator/protocol/openid-connect/token")
+					.then()
+					.statusCode(400).extract();
 		}
 	}
 }
