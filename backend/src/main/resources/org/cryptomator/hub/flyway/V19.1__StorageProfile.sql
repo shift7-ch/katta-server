3dfdcaf9b779df5828a12e40fb9d9a936cc5d5cd
chenkins <chenkins44@gmail.com>
2025-10-21 10:55:40 +0200

    Move bucket creation attributes one level up to allow bucket creation in Desktop client.

--- backend/src/main/resources/org/cryptomator/hub/flyway/V19.1__StorageProfile.sql
+++ backend/src/main/resources/org/cryptomator/hub/flyway/V19.1__StorageProfile.sql
@@ -23,17 +23,7 @@ CREATE TABLE "storage_profile_s3"
                         bool NOT NULL,
     "storageClass" VARCHAR NOT NULL,
 
-
-	CONSTRAINT "STORAGE_PROFILE_S3_PK" PRIMARY KEY ("id"),
-	CONSTRAINT "STORAGE_PROFILE_S3_FK_STORAGE_PROFILE" FOREIGN KEY ("id") REFERENCES "storage_profile" ("id") ON DELETE CASCADE,
-	CONSTRAINT "STORAGE_PROFILE_S3_CHK_STORAGE_CLASS" CHECK ("storageClass" = 'STANDARD' OR "storageClass" = 'INTELLIGENT_TIERING' OR "storageClass" = 'STANDARD_IA' OR "storageClass" = 'ONEZONE_IA' OR "storageClass" = 'GLACIER' OR "storageClass" = 'GLACIER_IR' OR "storageClass" = 'DEEP_ARCHIVE')
-);
-
-CREATE TABLE "storage_profile_s3_sts"
-(
-    "id"                UUID NOT NULL,
-
-    -- (2) bucket creation only (i.e. STS-case)
+  -- (2) bucket creation only (only relevant for Desktop client)
     "region"            VARCHAR,
     "regions"           text[],
     "bucketPrefix"      VARCHAR NOT NULL,
@@ -44,12 +34,23 @@ CREATE TABLE "storage_profile_s3_sts"
     "bucketAcceleration" bool,
     "bucketEncryption" VARCHAR NOT NULL,
 
+
+	CONSTRAINT "STORAGE_PROFILE_S3_PK" PRIMARY KEY ("id"),
+	CONSTRAINT "STORAGE_PROFILE_S3_FK_STORAGE_PROFILE" FOREIGN KEY ("id") REFERENCES "storage_profile" ("id") ON DELETE CASCADE,
+	CONSTRAINT "STORAGE_PROFILE_S3_CHK_STORAGE_CLASS" CHECK ("storageClass" = 'STANDARD' OR "storageClass" = 'INTELLIGENT_TIERING' OR "storageClass" = 'STANDARD_IA' OR "storageClass" = 'ONEZONE_IA' OR "storageClass" = 'GLACIER' OR "storageClass" = 'GLACIER_IR' OR "storageClass" = 'DEEP_ARCHIVE'),
+	CONSTRAINT "STORAGE_PROFILE_S3_CHK_BUCKET_ENCRYPTION" CHECK ("bucketEncryption" = 'NONE' OR "bucketEncryption" = 'SSE_AES256' OR "bucketEncryption" = 'SSE_KMS_DEFAULT')
+);
+
+CREATE TABLE "storage_profile_s3_sts"
+(
+    "id"                UUID NOT NULL,
+
     -- (3b) client profile custom properties
     "stsRoleArn"         VARCHAR NOT NULL,
     "stsRoleArn2"        VARCHAR,
     "stsDurationSeconds" INT4,
 
+
 	CONSTRAINT "STORAGE_PROFILE_S3_STS_PK" PRIMARY KEY ("id"),
-	CONSTRAINT "STORAGE_PROFILE_S3_STS_FK_STORAGE_PROFILE_S3" FOREIGN KEY ("id") REFERENCES "storage_profile_s3" ("id") ON DELETE CASCADE,
-	CONSTRAINT "STORAGE_PROFILE_S3_CHK_BUCKET_ENCRYPTION" CHECK ("bucketEncryption" = 'NONE' OR "bucketEncryption" = 'SSE_AES256' OR "bucketEncryption" = 'SSE_KMS_DEFAULT')
+	CONSTRAINT "STORAGE_PROFILE_S3_STS_FK_STORAGE_PROFILE_S3" FOREIGN KEY ("id") REFERENCES "storage_profile_s3" ("id") ON DELETE CASCADE
 );
