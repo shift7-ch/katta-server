4580387d82c201cfec43d7e6f68f2fd3d3233ee9
chenkins <chenkins44@gmail.com>
2025-11-13 09:45:29 +0100

    Cleanup.

--- backend/src/test/java/org/cryptomator/hub/cipherduck/TokenExchangeIT.java
+++ backend/src/test/java/org/cryptomator/hub/cipherduck/TokenExchangeIT.java
@@ -40,6 +40,44 @@ public class TokenExchangeIT {
 	@Test
 	@DisplayName("authenticate as public client 'cryptomator' and exchange token for 'cryptomatorvaults'")
 	public void testTokenExchange() throws GeneralSecurityException, IOException, InterruptedException {
+		// 1. Authenticate as public client using Authorization Code Flow with PKCE
+		var authResponse = TinyOAuth2.client("cryptomator") //
+				.withTokenEndpoint(URI.create(keycloakAuthServerUrl + "/protocol/openid-connect/token")) //
+				.authorizationCodeGrant(URI.create(keycloakAuthServerUrl + "/protocol/openid-connect/auth")) //
+				.authorize(HttpClient.newHttpClient(), uri -> {
+					try (var webClient = new WebClient()) {
+						webClient.setCssErrorHandler(new SilentCssErrorHandler());
+						HtmlPage page = webClient.getPage(uri.toASCIIString());
+						HtmlForm form = page.getForms().getFirst();
+						form.getInputByName("username").type("alice");
+						form.getInputByName("password").type("asd");
+						form.getInputByName("login").click();
+					} catch (IOException e) {
+						throw new UncheckedIOException(e);
+					}
+				}, "openid", "profile", "email"); // scopes of initial token
+		Assertions.assertEquals(200, authResponse.statusCode());
+		var initialAccessToken = new ObjectMapper().reader().readTree(authResponse.body()).get("access_token").asText();
+
+		// 2. Call the token exchange endpoint
+		var tokenExchangeResponse = RestAssured.given()
+				.auth().oauth2(initialAccessToken)
+				.queryParam("vault", "address") // "address" is one of cryptomatorvaults' optional client scope. In production there will be scopes for each vault
+				.post("/storage/s3-token");
+		Assertions.assertEquals(200, tokenExchangeResponse.statusCode());
+
+		var exchangedAccessToken = new ObjectMapper().reader().readTree(tokenExchangeResponse.body().asString()).get("access_token").asText();
+		var jwt = JWT.decode(exchangedAccessToken);
+		Assertions.assertEquals(1, jwt.getAudience().size());
+		Assertions.assertEquals("cryptomatorvaults", jwt.getAudience().getFirst());
+		Assertions.assertEquals("cryptomatorvaults", jwt.getClaim("azp").asString());
+		MatcherAssert.assertThat(jwt.getClaim("scope").asString(), Matchers.containsStringIgnoringCase("address"));
+	}
+
+
+	@Test
+	@DisplayName("Expired tokens returned from token exchange endpoint #94")
+	public void testReproduceExpiredTokens() throws GeneralSecurityException, IOException, InterruptedException {
 
 		// 0. set ssoSessionMaxLifespan to 5s
 		{
