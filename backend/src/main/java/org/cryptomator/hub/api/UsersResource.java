5e5e771af91f6730762045abc2f3f9db018723c0
chenkins <chenkins44@gmail.com>
2026-03-22 17:17:33 +0100

    Partial revert of 3c367a4163d77b87df486a8caa931c8e3ba55f84 - allow user role to GET users and groups.

--- backend/src/main/java/org/cryptomator/hub/api/UsersResource.java
+++ backend/src/main/java/org/cryptomator/hub/api/UsersResource.java
@@ -241,7 +241,7 @@ public class UsersResource {
 
 	@GET
 	@Path("/")
-	@RolesAllowed("admin")
+	@RolesAllowed({"admin", "user"})
 	@Produces(MediaType.APPLICATION_JSON)
 	@Transactional
 	@Operation(summary = "list all users with counts")
@@ -351,7 +351,7 @@ public class UsersResource {
 
 	@GET
 	@Path("/{id}")
-	@RolesAllowed("admin")
+	@RolesAllowed({"admin", "user"})
 	@Produces(MediaType.APPLICATION_JSON)
 	@NoCache
 	@Transactional
