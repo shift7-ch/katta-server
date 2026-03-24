c829976beb79bb32c1cf6067080520844752a50f
Christian Eichenberger <chenkins44@gmail.com>
2026-03-24 13:23:39 +0100

    Add missing api response description. (#112)
    * Partial revert of 3c367a4163d77b87df486a8caa931c8e3ba55f84 - allow user role to GET users and groups.
    
    * Fix typos in schema description.
    
    * Fix typos in schema description.
    
    * Revert "Partial revert of 3c367a4163d77b87df486a8caa931c8e3ba55f84 - allow user role to GET users and groups."
    
    This reverts commit 42a2c61178567fdb2337b191c2a571e1169b87f5.

--- backend/src/main/java/org/cryptomator/hub/api/cipherduck/StorageProfileS3StaticDto.java
+++ backend/src/main/java/org/cryptomator/hub/api/cipherduck/StorageProfileS3StaticDto.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
-@Schema(title = "StorageProfileS3Dto")
+@Schema(title = "StorageProfileS3StaticDto")
 public sealed class StorageProfileS3StaticDto extends StorageProfileDto permits StorageProfileS3STSDto {
 
 	public enum S3_STORAGE_CLASSES {
@@ -51,7 +51,7 @@ public sealed class StorageProfileS3StaticDto extends StorageProfileDto permits 
 	// (2) STS only: bucket creation  (only relevant for Desktop client)
 	//======================================================================
 	@JsonProperty(value = "region", required = true, defaultValue = "us-east-1")
-	@Schema(description = "Default region selected in the frontend/client to create bucket in.", example = "443", defaultValue = "us-east-1")
+	@Schema(description = "Default region selected in the frontend/client to create bucket in.", example = "us-east-1", defaultValue = "us-east-1")
 	String region = "us-east-1";
 
 	@JsonProperty(value = "regions", required = true)
@@ -59,7 +59,7 @@ public sealed class StorageProfileS3StaticDto extends StorageProfileDto permits 
 	List<String> regions = Region.regions().stream().map(Region::id).toList();
 
 	@JsonProperty(value = "bucketPrefix", required = true)
-	@Schema(description = "Buckets are create with name <bucket prefix><vault UUID>.", example = "cipherduck")
+	@Schema(description = "Buckets are created with name <bucket prefix><vault UUID>.", example = "cipherduck")
 	String bucketPrefix;
 
 	@JsonProperty(value = "stsRoleCreateBucketClient", required = true)
