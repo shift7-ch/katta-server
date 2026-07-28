8f827ca44ebe74683dbb63f24e52d042b023c3f1
Yves Langisch <yla@iterate.ch>
2026-07-28 14:21:44 +0200

    Review S3StorageHelper.

--- backend/src/main/java/org/cryptomator/hub/api/katta/storage/S3StorageHelper.java
+++ backend/src/main/java/org/cryptomator/hub/api/katta/storage/S3StorageHelper.java
@@ -1,6 +1,5 @@
 package org.cryptomator.hub.api.katta.storage;
 
-
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.ClientErrorException;
 import jakarta.ws.rs.core.Response;
@@ -14,91 +13,87 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3ClientBuilder;
 import software.amazon.awssdk.services.s3.S3Configuration;
-import software.amazon.awssdk.services.s3.model.CreateBucketConfiguration;
-import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
-import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
-import software.amazon.awssdk.services.s3.model.NoSuchBucketException;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.*;
 
 import java.net.URI;
 import java.util.Base64;
 
 @ApplicationScoped
 public class S3StorageHelper {
-	private static final Logger log = Logger.getLogger(S3StorageHelper.class);
+    private static final Logger log = Logger.getLogger(S3StorageHelper.class);
 
-	public void makeS3Bucket(
-			final StorageProfileS3STSDto storageConfig,
-			final CreateS3STSBucketDto dto
-	) {
+    public void makeS3Bucket(
+            final StorageProfileS3STSDto storageConfig,
+            final CreateS3STSBucketDto dto
+    ) {
 
-		if (log.isInfoEnabled()) {
-			log.info(String.format("Make S3 bucket %s for profile %s", dto, storageConfig));
-		}
+        if (log.isInfoEnabled()) {
+            log.info(String.format("Make S3 bucket %s for profile %s", dto, storageConfig));
+        }
 
-		final String bucketName = storageConfig.getBucketPrefix() + dto.vaultId();
-		// https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/java/example_code/s3/src/main/java/aws/example/s3/CreateBucket.java
-		final String region = dto.region();
+        final String bucketName = storageConfig.getBucketPrefix() + dto.vaultId();
+        // https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/java/example_code/s3/src/main/java/aws/example/s3/CreateBucket.java
+        final String region = dto.region();
 
-		S3ClientBuilder s3Builder = S3Client.builder()
-				.credentialsProvider(StaticCredentialsProvider.create(AwsSessionCredentials.create(dto.awsAccessKey(), dto.awsSecretKey(), dto.sessionToken())));
-		if (storageConfig.getStsEndpoint() != null) {
-			s3Builder = s3Builder
-					.endpointOverride(URI.create(storageConfig.getStsEndpoint()))
-					.serviceConfiguration(S3Configuration.builder()
-							.pathStyleAccessEnabled(storageConfig.isPathStyleAccessEnabled())
-							.build());
-		}
-		if (region != null) {
-			s3Builder = s3Builder.region(Region.of(region));
-		}
-		try (final S3Client s3 = s3Builder.build()) {
-			boolean okToCreate = true;
-			try {
-				s3.headBucket(HeadBucketRequest.builder().bucket(bucketName).build());
-			} catch (final NoSuchBucketException e) {
-				okToCreate = true;
-			} catch (final S3Exception e) {
-				if (e.statusCode() == 403) {
-					// ignore
-					log.info("Ignoring 403 on bucket head", e);
-					okToCreate = true;
-				}
-			}
-			if (!okToCreate) {
-				throw new ClientErrorException(String.format("Bucket %s already exists or no permission to list.", bucketName), Response.Status.CONFLICT);
-			}
+        S3ClientBuilder s3Builder = S3Client.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(AwsSessionCredentials.create(dto.awsAccessKey(), dto.awsSecretKey(), dto.sessionToken())));
+        if (storageConfig.getStsEndpoint() != null) {
+            s3Builder = s3Builder
+                    .endpointOverride(URI.create(storageConfig.getStsEndpoint()))
+                    .serviceConfiguration(S3Configuration.builder()
+                            .pathStyleAccessEnabled(storageConfig.isPathStyleAccessEnabled())
+                            .build());
+        }
+        if (region != null) {
+            s3Builder = s3Builder.region(Region.of(region));
+        }
+        try (final S3Client s3 = s3Builder.build()) {
+            try {
+                s3.headBucket(HeadBucketRequest.builder().bucket(bucketName).build());
+                throw new ClientErrorException(String.format("Bucket %s already exists", bucketName), Response.Status.CONFLICT);
+            } catch (final NoSuchBucketException e) {
+                // Ignore, as expected
+            } catch (final ClientErrorException e) {
+                throw e;
+            } catch (final Exception e) {
+                // Any other exception doesn't necessarily mean we can't create the bucket
+                log.warn(String.format("Failure heading bucket %s but try to create it", bucketName), e);
+            }
 
-			s3.createBucket(CreateBucketRequest.builder()
-					.bucket(bucketName)
-					.createBucketConfiguration(CreateBucketConfiguration.builder().locationConstraint(region).build())
-					.build());
-			if (log.isInfoEnabled()) {
-				log.info(String.format("Upload vault template to %s (%s, %s)", bucketName, dto, storageConfig));
-			}
-			s3.putObject(PutObjectRequest.builder()
-							.bucket(bucketName)
-							.key("vault.uvf")
-							.build(),
-					RequestBody.fromString(dto.vaultUvf()));
+            try {
+                s3.createBucket(CreateBucketRequest.builder()
+                        .bucket(bucketName)
+                        .createBucketConfiguration(CreateBucketConfiguration.builder().locationConstraint(region).build())
+                        .build());
 
-			// See https://github.com/cryptomator/hub/blob/develop/frontend/src/common/vaultconfig.ts
-			//        zip.file('vault.uvf', this.vaultUvf);
-			//        zip.folder('d')?.folder(this.rootDirHash.substring(0, 2))?.folder(this.rootDirHash.substring(2));
-			// create meta-data for your folder and set content-length to 0
-			final PutObjectRequest placeholderPutRequest = PutObjectRequest.builder()
-					.bucket(bucketName)
-					.key(String.format("d/%s/%s/", dto.rootDirHash().substring(0, 2), dto.rootDirHash().substring(2)))
-					.contentLength(0L)
-					.build();
-			s3.putObject(placeholderPutRequest, RequestBody.empty());
+                if (log.isInfoEnabled()) {
+                    log.info(String.format("Upload vault template to %s (%s, %s)", bucketName, dto, storageConfig));
+                }
+                s3.putObject(PutObjectRequest.builder()
+                                .bucket(bucketName)
+                                .key("vault.uvf")
+                                .build(),
+                        RequestBody.fromString(dto.vaultUvf()));
 
-			final PutObjectRequest dirUvfPutRequest = PutObjectRequest.builder()
-					.bucket(bucketName)
-					.key(String.format("d/%s/%s/dir.uvf", dto.rootDirHash().substring(0, 2), dto.rootDirHash().substring(2)))
-					.build();
-			s3.putObject(dirUvfPutRequest, RequestBody.fromBytes(Base64.getUrlDecoder().decode(dto.dirUvf())));
-		}
-	}
+                // See https://github.com/cryptomator/hub/blob/develop/frontend/src/common/vaultconfig.ts
+                //        zip.file('vault.uvf', this.vaultUvf);
+                //        zip.folder('d')?.folder(this.rootDirHash.substring(0, 2))?.folder(this.rootDirHash.substring(2));
+                // create meta-data for your folder and set content-length to 0
+                final PutObjectRequest placeholderPutRequest = PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(String.format("d/%s/%s/", dto.rootDirHash().substring(0, 2), dto.rootDirHash().substring(2)))
+                        .contentLength(0L)
+                        .build();
+                s3.putObject(placeholderPutRequest, RequestBody.empty());
+
+                final PutObjectRequest dirUvfPutRequest = PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(String.format("d/%s/%s/dir.uvf", dto.rootDirHash().substring(0, 2), dto.rootDirHash().substring(2)))
+                        .build();
+                s3.putObject(dirUvfPutRequest, RequestBody.fromBytes(Base64.getUrlDecoder().decode(dto.dirUvf())));
+            } catch (final Exception e) {
+                throw new ClientErrorException(String.format("Failure creating bucket %s and uploading vault template", bucketName), Response.Status.BAD_REQUEST, e);
+            }
+        }
+    }
 }
