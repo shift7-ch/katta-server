c6247dc4be60827e5dd30ede5fc35b1025c75f62
Yves Langisch <yla@iterate.ch>
2026-07-28 19:29:19 +0200

    Review exception handling.

--- backend/src/main/java/org/cryptomator/hub/api/katta/storage/S3StorageHelper.java
+++ backend/src/main/java/org/cryptomator/hub/api/katta/storage/S3StorageHelper.java
@@ -13,6 +13,7 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3ClientBuilder;
 import software.amazon.awssdk.services.s3.S3Configuration;
+import software.amazon.awssdk.services.s3.internal.BucketUtils;
 import software.amazon.awssdk.services.s3.model.*;
 
 import java.net.URI;
@@ -26,12 +27,14 @@ public class S3StorageHelper {
             final StorageProfileS3STSDto storageConfig,
             final CreateS3STSBucketDto dto
     ) {
-
         if (log.isInfoEnabled()) {
             log.info(String.format("Make S3 bucket %s for profile %s", dto, storageConfig));
         }
 
         final String bucketName = storageConfig.getBucketPrefix() + dto.vaultId();
+        if (!BucketUtils.isValidDnsBucketName(bucketName, false)) {
+            throw new ClientErrorException(String.format("Invalid bucket name %s ", bucketName), Response.Status.BAD_REQUEST);
+        }
         // https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/java/example_code/s3/src/main/java/aws/example/s3/CreateBucket.java
         final String region = dto.region();
 
@@ -49,51 +52,39 @@ public class S3StorageHelper {
         }
         try (final S3Client s3 = s3Builder.build()) {
             try {
-                s3.headBucket(HeadBucketRequest.builder().bucket(bucketName).build());
-                throw new ClientErrorException(String.format("Bucket %s already exists", bucketName), Response.Status.CONFLICT);
-            } catch (final NoSuchBucketException e) {
-                // Ignore, as expected
-            } catch (final ClientErrorException e) {
-                throw e;
-            } catch (final Exception e) {
-                // Any other exception doesn't necessarily mean we can't create the bucket
-                log.warn(String.format("Failure heading bucket %s but try to create it", bucketName), e);
-            }
-
-            try {
                 s3.createBucket(CreateBucketRequest.builder()
                         .bucket(bucketName)
                         .createBucketConfiguration(CreateBucketConfiguration.builder().locationConstraint(region).build())
                         .build());
-
-                if (log.isInfoEnabled()) {
-                    log.info(String.format("Upload vault template to %s (%s, %s)", bucketName, dto, storageConfig));
-                }
-                s3.putObject(PutObjectRequest.builder()
-                                .bucket(bucketName)
-                                .key("vault.uvf")
-                                .build(),
-                        RequestBody.fromString(dto.vaultUvf()));
-
-                // See https://github.com/cryptomator/hub/blob/develop/frontend/src/common/vaultconfig.ts
-                //        zip.file('vault.uvf', this.vaultUvf);
-                //        zip.folder('d')?.folder(this.rootDirHash.substring(0, 2))?.folder(this.rootDirHash.substring(2));
-                // create meta-data for your folder and set content-length to 0
-                final PutObjectRequest placeholderPutRequest = PutObjectRequest.builder()
-                        .bucket(bucketName)
-                        .key(String.format("d/%s/%s/", dto.rootDirHash().substring(0, 2), dto.rootDirHash().substring(2)))
-                        .contentLength(0L)
-                        .build();
-                s3.putObject(placeholderPutRequest, RequestBody.empty());
-
-                final PutObjectRequest dirUvfPutRequest = PutObjectRequest.builder()
-                        .bucket(bucketName)
-                        .key(String.format("d/%s/%s/dir.uvf", dto.rootDirHash().substring(0, 2), dto.rootDirHash().substring(2)))
-                        .build();
-                s3.putObject(dirUvfPutRequest, RequestBody.fromBytes(Base64.getUrlDecoder().decode(dto.dirUvf())));
-            } catch (final Exception e) {
-                throw new ClientErrorException(String.format("Failure creating bucket %s and uploading vault template", bucketName), Response.Status.BAD_REQUEST, e);
+            } catch (final BucketAlreadyOwnedByYouException | BucketAlreadyExistsException e) {
+                throw new ClientErrorException(String.format("Bucket %s already exists", bucketName), Response.Status.CONFLICT, e);
             }
+
+            if (log.isInfoEnabled()) {
+                log.info(String.format("Upload vault template to %s (%s, %s)", bucketName, dto, storageConfig));
+            }
+            s3.putObject(PutObjectRequest.builder()
+                            .bucket(bucketName)
+                            .key("vault.uvf")
+                            .build(),
+                    RequestBody.fromString(dto.vaultUvf()));
+
+            // See https://github.com/cryptomator/hub/blob/develop/frontend/src/common/vaultconfig.ts
+            //        zip.file('vault.uvf', this.vaultUvf);
+            //        zip.folder('d')?.folder(this.rootDirHash.substring(0, 2))?.folder(this.rootDirHash.substring(2));
+            // create meta-data for your folder and set content-length to 0
+            final PutObjectRequest placeholderPutRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(String.format("d/%s/%s/", dto.rootDirHash().substring(0, 2), dto.rootDirHash().substring(2)))
+                    .contentLength(0L)
+                    .build();
+            s3.putObject(placeholderPutRequest, RequestBody.empty());
+
+            final PutObjectRequest dirUvfPutRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(String.format("d/%s/%s/dir.uvf", dto.rootDirHash().substring(0, 2), dto.rootDirHash().substring(2)))
+                    .build();
+            s3.putObject(dirUvfPutRequest, RequestBody.fromBytes(Base64.getUrlDecoder().decode(dto.dirUvf())));
         }
     }
 }
